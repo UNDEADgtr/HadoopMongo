91f1ce65740814268df9a0df46c538c4b73e99d0
UNDEADgtr <undead_gtr@mail.ru>
2013-05-23 11:30:06 +0300

    Added import file

--- src/main/java/nl/jnc/util/Util.java
+++ src/main/java/nl/jnc/util/Util.java
@@ -6,8 +6,8 @@ import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
 import nl.jnc.PatentData;
 
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -22,36 +22,46 @@ public class Util {
 
     public void importToBase() throws IOException {
 
-        Scanner scanner = new Scanner(new FileReader("/home/dzmity/IdeaProjects/HadoopTest/src/main/resources/nl/nl.jnc/nl.nl.jnc.util/apat.txt"));
+        //Scanner scanner = new Scanner(new FileReader("/home/dzmity/IdeaProjects/HadoopTest/src/main/resources/nl/jnc/util/apat.txt"));
+
+        Class clazz = this.getClass();
+        InputStream stream;
+        Scanner scanner;
+
         Mongo mongo = new MongoClient();
         DBCollection collection = mongo.getDB("test").getCollection("in");
-        scanner.nextLine();
         int count = 0;
         System.out.println("Start import");
-        List<DBObject> list = new ArrayList<DBObject>(1000);
-        while (scanner.hasNextLine()) {
-            String[] columns = scanner.nextLine().split(",");
-            PatentData patent = new PatentData();
-            patent.setPatent(columns[0]);
-            patent.setYear(columns[1]);
-            patent.setCountry(columns[4].replace("\"", ""));
-            patent.setCode(columns[7]);
-            list.add(patent.getDBObject());
-            if (list.size() == 1000) {
-                collection.insert(list);
-                list.clear();
+        long startTime = System.nanoTime();
+
+        List<DBObject> list = new ArrayList<DBObject>(10000);
+
+        for (int i = 0; i < 10; i++) {
+            stream = clazz.getResourceAsStream("apat.txt");
+            scanner = new Scanner(stream);
+            scanner.nextLine();
+            while (scanner.hasNextLine()) {
+                String[] columns = scanner.nextLine().split(",");
+                PatentData patent = new PatentData();
+                patent.setPatent(columns[0]);
+                patent.setYear(columns[1]);
+                patent.setCountry(columns[4].replace("\"", ""));
+                patent.setCode(columns[7]);
+                list.add(patent.getDBObject());
+                if (list.size() == 10000) {
+                    collection.insert(list);
+                    list.clear();
+                }
+                count++;
             }
-            count++;
         }
+
+        long endMapReduceTime = System.nanoTime();
+        double mapReduceTime = (endMapReduceTime - startTime) / 1000000000;
+
         System.out.println("Finish import");
         System.out.println(String.format("Imported %s records", count));
-//        InputStream is = Util.class.getResourceAsStream("test.xml");
-//        InputStream stream = this.getClass().getResourceAsStream("test.xml");
-//        InputStreamReader reader = new InputStreamReader(stream);
-//        BufferedReader buff = new BufferedReader(reader);
-//        String s = buff.readLine();
-//        while ((s = buff.readLine()) != null) {
-//        }
+        System.out.println(String.format("Import  time = %s c", mapReduceTime));
     }
 
 }
